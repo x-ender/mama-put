1a2d07f5a1a8d0328b32463ee34aa98b446a45b0
Nkwo Chimdike <chimdinkwo@gmail.com>
2020-10-13 13:04:00 +0100

    required and initialized mongoose.. and established connection with the local mongoDB

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 // Init express
 const app = express();
-
 // PORT
 const PORT = process.env.PORT || 5000;
+
+// Init Mongoose for mongoDB
+mongoose.connect('mongodb://localhost:27017/mamaput', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+});
+
+// Establishing Connection
+const connection = mongoose.connection;
+connection.once('open', () => {
+    console.log("Mongodb connected");
+})
+
+
+
 
 // Homepage Route
 app.get('/', (req, res) => {
